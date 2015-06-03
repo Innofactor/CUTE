4d38a631c731159954ec96befcff45a8e98ac0b1
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-06-03 15:01:26 +0200

    Adding NoInput tests for Service

--- Tests/Service/NoInputTests.cs
+++ Tests/Service/NoInputTests.cs
@@ -1,37 +1,36 @@
 ﻿namespace Cinteros.Unit.Test.Extensions.Tests.Service
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     using Cinteros.Unit.Test.Extensions.Core;
+    using Cinteros.Unit.Test.Extensions.Core.Background;
     using Microsoft.Xrm.Sdk;
     using Microsoft.Xrm.Sdk.Query;
-    using NSubstitute;
     using Xunit;
 
-    public class SerializedInputTests : CoreTests
+    public class NoInputTests : CoreTests
     {
-        #region Public Constructors
-
-        public SerializedInputTests()
-            : base ()
+        public NoInputTests()
         {
-            // Calling methods to fill calls stack
-            this.Service.Create(new Entity());
-            this.Service.Retrieve(string.Empty, Guid.Empty, new ColumnSet());
-            this.Service.RetrieveMultiple(new QueryExpression());
-            this.Service.Execute(new OrganizationRequest());
+            // Creating provider from scratch
+            this.Provider = new CuteProvider();
 
-            // Recreating provider from serialized one
-            this.Provider = new CuteProvider(this.Provider.ToString());
+            this.Provider.Calls.Add(new CuteCall(MessageName.Create, new[] { new Entity() }, this.expectedResultCreate));
+            this.Provider.Calls.Add(new CuteCall(MessageName.Retrieve, new object[] { string.Empty, Guid.Empty, new ColumnSet() }, this.expectedResultRetrieve));
+            this.Provider.Calls.Add(new CuteCall(MessageName.RetrieveMultiple, new object[] { new QueryExpression() }, this.expectedResultRetrieveMultiple));
+            this.Provider.Calls.Add(new CuteCall(MessageName.Execute, new object[] { new OrganizationRequest() }, this.expectedResultExecute));
+
             this.Service = ((IOrganizationServiceFactory)this.Provider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(Guid.Empty);
         }
-
-        #endregion Public Constructors
 
         #region Public Methods
 
         [Fact(DisplayName = "Invoke Associate")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Associate()
         {
             base.Invoke_Associate();
@@ -39,7 +38,7 @@
 
         [Fact(DisplayName = "Invoke Create & Check Cache")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Create_Check_Cache()
         {
             base.Invoke_Create_Check_Cache();
@@ -47,7 +46,7 @@
 
         [Fact(DisplayName = "Invoke Delete")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Delete()
         {
             base.Invoke_Delete();
@@ -55,7 +54,7 @@
 
         [Fact(DisplayName = "Invoke Disassociate")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Disassociate()
         {
             base.Invoke_Disassociate();
@@ -63,7 +62,7 @@
 
         [Fact(DisplayName = "Invoke Execute & Check Cache")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Execute_Check_Cache()
         {
             base.Invoke_Execute_Check_Cache();
@@ -71,7 +70,7 @@
 
         [Fact(DisplayName = "Invoke Retrieve & Check Cache")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Retrieve_Check_Cache()
         {
             base.Invoke_Retrieve_Check_Cache();
@@ -79,7 +78,7 @@
 
         [Fact(DisplayName = "Invoke RetrieveMultiple & Check Cache")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_RetrieveMultiple_Check_Cache()
         {
             base.Invoke_RetrieveMultiple_Check_Cache();
@@ -87,7 +86,7 @@
 
         [Fact(DisplayName = "Invoke Update")]
         [Trait("Module", "Service")]
-        [Trait("Provider", "Serialized Input")]
+        [Trait("Provider", "No Input")]
         public override void Invoke_Update()
         {
             base.Invoke_Update();
