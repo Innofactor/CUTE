a8a626862dea7c5b029d21daf6d4f9516a4377da
Alexey Shytikov <alexey.shytikov@cinteros.se>
2015-06-10 15:17:56 +0200

    Switching unit tests to NUnit

--- Tests/Provider/WrappedInputTests.cs
+++ Tests/Provider/WrappedInputTests.cs
@@ -1,63 +1,59 @@
 ﻿namespace Cinteros.Unit.Testing.Extensions.Tests.Provider
 {
     using Cinteros.Unit.Testing.Extensions.Core;
-    using Microsoft.Xrm.Sdk;
-    using Xunit;
+    using NUnit.Framework;
 
     public class WrappedInputTests : CoreTests
     {
-        #region Public Constructors
-
-        public WrappedInputTests()
-            : base()
-        {
-            this.Provider = new CuteProvider(this.Provider);
-        }
-
-        #endregion Public Constructors
-
         #region Public Methods
 
-        [Fact(DisplayName = "Check Online Status")]
-        [Trait("Module", "Provider")]
-        [Trait("Provider", "Wrapped Input")]
+        [Test]
+        [Category("Provider")]
+        [Category("Wrapped Input")]
         public override void Check_Online_Status()
         {
             base.Check_Online_Status();
         }
 
-        [Fact(DisplayName = "Get Context")]
-        [Trait("Module", "Provider")]
-        [Trait("Module", "Context")]
-        [Trait("Provider", "Wrapped Input")]
+        [Test]
+        [Category("Provider")]
+        [Category("Wrapped Input")]
+        [Category("Context")]
         public override void Get_Context()
         {
             base.Get_Context();
         }
 
-        [Fact(DisplayName = "Get OriginalProvider")]
-        [Trait("Module", "Provider")]
-        [Trait("Provider", "Wrapped Input")]
+        [Test]
+        [Category("Provider")]
+        [Category("Wrapped Input")]
         public override void Get_OriginalProvider()
         {
             base.Get_OriginalProvider();
         }
 
-        [Fact(DisplayName = "Get TracingService")]
-        [Trait("Module", "Provider")]
-        [Trait("Provider", "Wrapped Input")]
+        [Test]
+        [Category("Provider")]
+        [Category("Wrapped Input")]
         public override void Get_TracingService()
         {
             base.Get_TracingService();
         }
 
-        [Fact(DisplayName = "Get WrappedFactory")]
-        [Trait("Module", "Provider")]
-        [Trait("Module", "Factory")]
-        [Trait("Provider", "Wrapped Input")]
+        [Test]
+        [Category("Provider")]
+        [Category("Wrapped Input")]
+        [Category("Factory")]
         public override void Get_WrappedFactory()
         {
             base.Get_WrappedFactory();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            base.Setup();
+            this.Provider = new CuteProvider(this.Provider);
         }
 
         #endregion Public Methods
